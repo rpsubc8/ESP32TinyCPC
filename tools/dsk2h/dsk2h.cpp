7a393a709c9ed1ac1a04fba01dc83c435cd3569f
ackerman <jaime_jj@hotmail.com>
2022-01-02 18:17:09 +0100

    Load and run roms (refactor dsk)

--- tools/dsk2h/dsk2h.cpp
+++ tools/dsk2h/dsk2h.cpp
@@ -1,5 +1,6 @@
 //Author: ackerman
-//Convert dsks .dsk to .h Tiny ESP32 CPC emulator
+//Convert dsks .dsk to .h and .rom to .h Tiny ESP32 CPC emulator
+//Longitud de titulo de ficheros maxima 12 caracteres
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
@@ -7,9 +8,17 @@
 
 #define maxDSK 255
 
+#define fichDSK 0
+#define fichROM 1
+
+#define maxLengthFileName 12
+
+unsigned char gbTipoFichero= fichDSK;
+
 unsigned char discsects[40];
 unsigned char discid[42][11][4];
 unsigned char discdat[42][11][512];
+unsigned char gb_datROMEXTRA[65535]; //El maximo es 16384 16 KB
 int disctracks;
 
 
@@ -17,8 +26,12 @@ int disctracks;
 
 void listFilesRecursively(char *path);
 void ShowHelp(void);
-void WriteHeadH(char *titleArray, unsigned char totalTitle);
-void WriteFileRom(unsigned char num,char *cadPath, char *cadFile);
+void WriteHeadH_DSK(char *titleArray, unsigned char totalTitle);
+void WriteHeadH_ROMEXTRA(char *titleArray, unsigned char totalTitle);
+void WriteFileDSK(unsigned char num,char *cadPath, char *cadFile);
+void WriteFileROMEXTRA(unsigned char num,char *cadPath, char *cadFile, int aSize);
+int loadROMEXTRA(char *cadPath,char *discname);
+void WriteHexROMEXTRA(FILE *fileOut,char *cadFile,int aSize);
 //int GetSizeFile(char *cadFile);
 void InitTitles(void);
 void loaddsk(char *cadPath,char *discname);
@@ -35,10 +48,10 @@ unsigned char gb_contRom=0;
 void RemoveExt(char *cad)
 {
  int total= strlen(cad);
- if (total>8)
+ if (total>maxLengthFileName)
  {
-  total= 8;
-  cad[8]='\0';
+  total= maxLengthFileName;
+  cad[maxLengthFileName]='\0';
  }
  for (int i=0;i<total;i++)
  {
@@ -202,7 +215,7 @@ void WriteHexDiscdat(FILE *fileOut,char *cadFile)
 
 
 //**********************************************
-void WriteFileRom(unsigned char num,char *cadPath,char *cadFile)
+void WriteFileDSK(unsigned char num,char *cadPath,char *cadFile)
 {
  unsigned char contLine=0;
  FILE *auxWrite = NULL;
@@ -211,7 +224,7 @@ void WriteFileRom(unsigned char num,char *cadPath,char *cadFile)
  char cadDest[2048];
  char cadOri[2048];
  sprintf(cadOri,"%s\\%s",cadPath,cadFile);
- sprintf(cadDest,"dataFlash//dsk//dsk%s.h",cadFile);
+ sprintf(cadDest,"output//dataFlash//dsk//dsk%s.h",cadFile);
  auxWrite = fopen(cadDest,"w+");
  if (auxWrite!= NULL)
  {
@@ -254,9 +267,138 @@ void WriteFileRom(unsigned char num,char *cadPath,char *cadFile)
  }
 }
 
+//**************************************************
+int sizeFile(char *cadFile)
+{
+ int aReturn=0;
+ FILE *f=fopen(cadFile,"rb");
+ if (f!= NULL)
+ {
+  fseek(f, 0, SEEK_END);
+  aReturn = ftell(f);
+  fseek(f, 0, SEEK_SET); 
+  fclose(f);
+ }
+ return aReturn;
+}
+
+//**************************************************
+int loadROMEXTRA(char *cadPath,char *discname)
+{
+ int aReturnSize=0;
+ char cadOri[2048];
+ sprintf(cadOri,"%s\\%s",cadPath,discname); 
+
+ aReturnSize= sizeFile(cadOri);
+      
+ FILE *f=fopen(cadOri,"rb");
+ if (!f) return 0;
+ //fread(gb_datROMEXTRA,16384,1,f);
+ fread(gb_datROMEXTRA,aReturnSize,1,f);
+ fclose(f);
+ 
+ return aReturnSize;
+}
+
+//********************************************************
+void WriteHexROMEXTRA(FILE *fileOut,char *cadFile, int aSize)
+{//Escribe HEX ROMEXTRA
+ int i;
+ int cont=0;
+ //int tope=16384;
+ int tope=aSize;
+ int contInterno=0;
+ fprintf(fileOut,"const unsigned char gb_romextra_dat_%s[]={\n",cadFile);
+ for (int i=0;i<tope;i++)
+ {
+  fprintf(fileOut,"0x%02x",gb_datROMEXTRA[i]);
+  if (contInterno<tope)
+   fprintf(fileOut,",");
+  contInterno++; 
+  cont++;
+  if (cont>15) {cont=0; fprintf(fileOut,"\n");}
+ }
+ fprintf(fileOut,"\n};\n\n");    
+}
+
 //**********************************************
-void WriteHeadH(char *titleArray, unsigned char totalTitle)
-{//Cabecera las 10 roms
+void WriteFileROMEXTRA(unsigned char num,char *cadPath,char *cadFile, int aSize)
+{
+ unsigned char contLine=0;
+ FILE *auxWrite = NULL;
+ FILE *auxRead = NULL;
+ long auxSize=0;
+ char cadDest[2048];
+ char cadOri[2048];
+ sprintf(cadOri,"%s\\%s",cadPath,cadFile);
+ sprintf(cadDest,"output//dataFlash//romextra//romextra%s.h",cadFile);
+ auxWrite = fopen(cadDest,"w+");
+ if (auxWrite!= NULL)
+ {
+  fprintf(auxWrite,"#ifndef ROMEXTRA_%s_H\n",cadFile);
+  fprintf(auxWrite," #define ROMEXTRA_%s_H\n",cadFile);   
+  //fprintf(auxWrite,"//ROMEXTRA %s 16384 bytes\n\n",cadFile);
+  fprintf(auxWrite,"//ROMEXTRA %s %d bytes\n\n",cadFile,aSize);
+  
+  WriteHexROMEXTRA(auxWrite,cadFile,aSize);
+  //WriteHexSects(auxWrite,cadFile);
+  //WriteHexDiscid(auxWrite,cadFile);
+  //WriteHexDiscdat(auxWrite,cadFile);
+  
+  fprintf(auxWrite,"#endif\n");
+  fclose(auxWrite);
+ }
+}
+
+//**********************************************
+void WriteHeadH_ROMEXTRA(char *titleArray, unsigned char totalTitle)
+{
+ if (gb_fileWrite == NULL)
+  return;
+ fprintf(gb_fileWrite,"#ifndef _GBROMEXTRA_H\n");
+ fprintf(gb_fileWrite," #define _GBROMEXTRA_H\n"); 
+ fprintf(gb_fileWrite,"\n");
+ for (unsigned char i=0;i<gb_contRom;i++)
+ {
+  if (strlen(gb_titles[i]) != 0)
+   fprintf(gb_fileWrite," #include \"romextra/romextra%s.h\"\n",gb_titles[i]);
+ }
+ fprintf(gb_fileWrite,"\n");
+ fprintf(gb_fileWrite," #define max_list_romextra %d\n\n",gb_contRom);
+ fprintf(gb_fileWrite," static const char * gb_list_romextra_title[max_list_romextra]={\n");
+ for (unsigned char i=0;i<gb_contRom;i++)
+ {
+  if (strlen(gb_titles[i]) == 0)
+   fprintf(gb_fileWrite,"  \"\"");
+  else 
+   fprintf(gb_fileWrite,"  \"%s\"",gb_titles[i]);
+  if (i<(gb_contRom-1))
+   fprintf(gb_fileWrite,",\n");
+ }
+ fprintf(gb_fileWrite,"\n };\n");
+ fprintf(gb_fileWrite,"\n"); 
+
+ fprintf(gb_fileWrite,"\n");
+ fprintf(gb_fileWrite," static const unsigned char * gb_list_romextra_data[max_list_romextra]={\n");
+ for (unsigned char i=0;i<gb_contRom;i++)
+ {
+  if (strlen(gb_titles[i]) == 0)
+   fprintf(gb_fileWrite,"  NULL");
+  else 
+   fprintf(gb_fileWrite,"  gb_romextra_dat_%s",gb_titles[i]);
+  if (i<(gb_contRom-1))
+   fprintf(gb_fileWrite,",\n");   
+ }  
+ fprintf(gb_fileWrite,"\n };\n");  
+ 
+ 
+ fprintf(gb_fileWrite,"\n");
+ fprintf(gb_fileWrite,"#endif\n");     
+}
+
+//**********************************************
+void WriteHeadH_DSK(char *titleArray, unsigned char totalTitle)
+{//Cabecera 255 DSKs
  if (gb_fileWrite == NULL)
   return;
  fprintf(gb_fileWrite,"#ifndef _GBDSK_H\n");
@@ -327,9 +469,10 @@ void WriteHeadH(char *titleArray, unsigned char totalTitle)
 //**********************************************
 void listFilesRecursively(char *basePath)
 {
+    int auxSizeFileROM=0;
     char path[1000];
     struct dirent *dp;
-    DIR *dir = opendir(basePath);
+    DIR *dir = opendir(basePath);    
     if (!dir)
         return; // Unable to open directory stream
     while ((dp = readdir(dir)) != NULL)
@@ -338,11 +481,25 @@ void listFilesRecursively(char *basePath)
         {
             printf("%s\n", dp->d_name);
 
-            ClearDSK();
-            loaddsk(basePath,dp->d_name);
+            switch (gbTipoFichero)
+            {
+             case fichDSK: 
+              ClearDSK();
+              loaddsk(basePath,dp->d_name);
+              break;
+             case fichROM:
+              memset(gb_datROMEXTRA,0,65535); //Reset buffer rom
+              auxSizeFileROM = loadROMEXTRA(basePath,dp->d_name);
+              break;
+            }
             RemoveExt(dp->d_name);            
             strcpy(gb_titles[gb_contRom],dp->d_name);
-            WriteFileRom(gb_contRom,basePath,dp->d_name);            
+            switch (gbTipoFichero)
+            {
+             case fichDSK: WriteFileDSK(gb_contRom,basePath,dp->d_name); break;
+             case fichROM: WriteFileROMEXTRA(gb_contRom,basePath,dp->d_name,auxSizeFileROM); break;
+            }
+            
                         
             gb_contRom++;
             if (gb_contRom > (maxDSK-1))
@@ -365,14 +522,32 @@ int main(int argc, char**argv)
  char path[100];    //Directory path to list files
  ShowHelp();
 
+ //Procesar DSKS
  InitTitles();
- gb_fileWrite = fopen("dataFlash//gbdsk.h","w+");
+ gb_fileWrite = fopen("output//dataFlash//gbdsk.h","w+");
  if (gb_fileWrite!= NULL)
  {
-  listFilesRecursively("dsks");
-  WriteHeadH(NULL,gb_contRom);
+  printf("DSK files\n");
+  gbTipoFichero= fichDSK;
+  gb_contRom= 0; //Reset contador ficheros
+  listFilesRecursively("input//dsks");
+  WriteHeadH_DSK(NULL,gb_contRom);
   fclose(gb_fileWrite);
  }
+ 
+ //Procesar ROMS EXTRA
+ InitTitles();
+ gb_fileWrite = fopen("output//dataFlash//gbromextra.h","w+");
+ if (gb_fileWrite!= NULL)
+ {
+  printf("\nROMEXTRA files\n");
+  gbTipoFichero= fichROM;                      
+  gb_contRom= 0; //Reset contador ficheros
+  listFilesRecursively("input//romextra");
+  WriteHeadH_ROMEXTRA(NULL,gb_contRom);
+  fclose(gb_fileWrite);
+ } 
+ 
   //printf("Enter path to list files: ");    // Input path from user
   //scanf("%s", path);
   //listFilesRecursively(path);
